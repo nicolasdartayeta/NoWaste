83227ead47691cad922cb36e6108b6beb65fa225
Nicolas Dartayeta <63559546+nicolasdartayeta@users.noreply.github.com>
2024-05-27 13:30:29 -0300

    carpeta de imagenes agregada

--- my-project/controllers/restauranteController.js
+++ my-project/controllers/restauranteController.js
@@ -8,6 +8,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {         //CHECK SI ESTA LA CARPETA O NO
     cb(null, './public/images')
   },
+  
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     cb(null, file.fieldname + '-' + uniqueSuffix + '.jpg')
