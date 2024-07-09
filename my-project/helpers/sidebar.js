bd0a7c240878f95c42313045892ba663dd41c60c
Nicolas Dartayeta <63559546+nicolasdartayeta@users.noreply.github.com>
2024-07-09 12:04:52 -0300

    merge conflict

--- my-project/helpers/sidebar.js
+++ my-project/helpers/sidebar.js
@@ -8,7 +8,6 @@ class sidebarModel {
     }
 
     addItem(title, requestURL, target, id, deletable){
-        console.log(deletable)
         let item = {
             "title": title,
             "requestURL": requestURL,
@@ -18,7 +17,6 @@ class sidebarModel {
             item.id = id
         }
         item.deletable = deletable
-        // console.log(item)
         this.#content.items.push(item)
     }
 
@@ -30,7 +28,6 @@ class sidebarModel {
 async function sidebarRestaurantes(baseURL, deletable="false") {
     const restaurantes = await restauranteModel.find().exec()
     const sidebar = new sidebarModel('Lista de comercios')
-    console.log(deletable)
     restaurantes.forEach((restaurante) => sidebar.addItem(  restaurante.nombre, 
                                                             `${baseURL}/show/${restaurante._id}`, 
                                                             "#content", restaurante._id.toString(),
